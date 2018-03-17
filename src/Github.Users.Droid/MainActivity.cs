dd48cfb5164c64beccaa25c54f1b4fd28621c520
Rafal Pienkowski <rafal.pienkowski@wp.pl>
2018-03-17 16:02:12 +0100

    Android app.

---
+++ src/Github.Users.Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Github.Users.Common.ViewModels;
+
+namespace Github.Users.Droid
+{
+    [Activity(Label = "Github.Users.Droid", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : ListActivity
+    {
+        int count = 1;
+
+        protected async override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            ListAdapter = await CreateUsersListAdapater();
+        }
+
+        private async Task<UsersListAdapater> CreateUsersListAdapater()
+        {
+            var usersViewModel = new UsersViewModel();
+            await usersViewModel.RetrieveUsers();
+
+            return new UsersListAdapater(this, usersViewModel.Users);
+        }
+    }
+}
+
---
+++ src/Github.Users.Droid/UsersListAdapater.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Github.Users.Common.Models;
+using Github.Users.Common.Utils;
+
+namespace Github.Users.Droid
+{
+    public class UsersListAdapater : BaseAdapter<User>
+    {
+        private readonly IEnumerable<User> _users;
+        private Activity _context;
+
+        public UsersListAdapater(Activity context, IEnumerable<User> users) : base()
+        {
+            context.IsNull();
+            users.IsNull();
+
+            _context = context;
+            _users = users;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return this[position].Id;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var user = this[position];
+
+            var view = convertView ?? _context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem2, null);
+
+            view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = user.Name;
+            view.FindViewById<TextView>(Android.Resource.Id.Text2).Text = user.Company.Name;
+
+            return view;
+        }
+
+        public override int Count => _users.Count();
+
+        public override User this[int position] => _users.ElementAt(position);
+    }
+}
